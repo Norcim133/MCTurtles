a86a0016bee8a6626b463d52df0999c54934a59b
ihill9 <ihill9@gatech.edu>
2023-01-14 16:39:58 +1100

    Improved miner

--- NewMine.lua
+++ NewMine.lua
@@ -1,1 +1,32 @@
-print("This will be a mining program")
+local returnSteps=0
+
+local torch=0
+
+for i=1, 100 do
+
+    while turtle.dig() do
+        print("Digging forward")
+    end
+
+    if turtle.forward() then
+        returnSteps = returnSteps + 1
+        while turtle.digUp() do
+            print("Digging")
+        end
+        if torch == 6 then
+            turtle.turnLeft()
+            turtle.placeUp() --Need to check for torch
+            turtle.turnRight()
+            torch=0
+        end
+    end
+
+    torch = torch + 1
+end
+
+turtle.turnLeft()
+turtle.turnLeft()
+
+for j=1, returnSteps do
+    turtle.forward()
+end
