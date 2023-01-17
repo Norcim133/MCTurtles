87a829dabdf95e16a64488b2c0585078f4c1aa47
ihill9 <ihill9@gatech.edu>
2023-01-17 22:03:10 +1100

    Redoing OreSearch

--- NewMine.lua
+++ NewMine.lua
@@ -1,33 +1,106 @@
-require("OreSearch")
-local returnSteps=0
+function forward()
+    while not turtle.forward() do
+        turtle.dig()
+    end
+    return true
+end
 
-local torch=0
+function up()
+    while not turtle.Up() do
+        turtle.digUp()
+    end
+    return true
+end
 
-for i=1, 400 do
+function down()
+    while not turtle.Down() do
+        turtle.digDown()
+    end
+    return true
+end
 
-    if forward() then
-        returnSteps = returnSteps + 1
-        checkChildren(ore)
-        while turtle.digUp() do
-            print("Digging")
-        end
-        if torch == 6 then
-            turnLeft()
-            turtle.placeUp() --Need to check for torch
-            turnRight()
-            torch=0
+function turnRight()
+    if turtle.turnRight() then
+        return true
+    else
+        return false
+    end
+end
+
+function turnLeft()
+    if turtle.turnLeft() then
+        return true
+    else
+        return false
+    end
+end
+
+function turnaround()
+    turtle.turnRight()
+    turtle.turnRight()
+end
+
+function back()
+    turnaround()
+    forward()
+    turnaround()
+end
+
+function checkPlanar(ore)
+    local success, data = turtle.inspect()
+    if success then
+        if data.name == ore then
+            forward()
+            checkChildren(ore)
+            back()
         end
     end
-
-    torch = torch + 1
 end
 
-turnLeft()
-turnLeft()
+function checkChildren(ore)
 
-for j=1, returnSteps do
-    forward()
+    children = {"forward", "right", "left", "up", "down"}
+
+    for child in children do
+        if child == "forward" then
+            checkPlanar(ore)
+        end
+
+        if child == "right" then
+            turnRight()
+            checkPlanar(ore)
+            turnLeft()
+        end
+
+        if child == "left" then
+            turnLeft()
+            checkPlanar(ore)
+            turnRight()
+        end
+
+        if child == "up" then
+            local success, data = turtle.inspectUp()
+            if success then
+                if data.name == ore then
+                    up()
+                    checkChildren(ore)
+                    down()
+                end
+            end
+        end
+
+        if child == "down" then
+            local success, data = turtle.inspectDown()
+            if success then
+                if data.name == ore then
+                    down()
+                    checkChildren(ore)
+                    up()
+                end
+            end
+        end
+
+
+    end
+
 end
-
-args = {...}
-ore = (args[1])
